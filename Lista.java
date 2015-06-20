7bbd1c73e1fa6bd33d78a551de86eb7d4692f939
Patrick Weigle <patrickweiglecrf@gmail.com>
2015-06-20 15:31:35 -0300

    Tentando adicionar corpo

--- Lista.java
+++ Lista.java
@@ -9,16 +9,13 @@ public class Lista
 {
     
     private Snake inicio;
-    Snake corpo = new Snake();
     
-        public void inserirInicio(Snake _node){
-        if(isEmpty()){
-            inicio = _node;
-        }else{  
-            _node.setProximo(inicio);
-            inicio = _node;
-        }
+    
+    public Lista(){
+        this.inicio = new Snake();
     }
+    
+    
     public boolean isEmpty(){
         if(inicio == null){
             return true;
@@ -27,15 +24,55 @@ public class Lista
         }
     }
     
-    public void inserirFinal(Snake _node){
-        if(isEmpty()){
-            inicio = _node;
+    public void inserirFinal(){
+        if(inicio.getProximo() == null){
+            
+            Snake corpo = inicio.novaSnake();
+            corpo.setX(inicio.getX()+50);
+            corpo.setY(inicio.getY());
+            inicio.setProximo(corpo);
         }else{
             Snake aux = inicio;
+            Snake novo = aux.novaSnake();
             while(aux.getProximo() != null){
                 aux = aux.getProximo();
             }
-            aux.setProximo(_node);
+            novo.setX(aux.getX()+50);
+            novo.setY(aux.getY());
+            aux.setProximo(novo);
         }
+    }
+    
+    public Snake ultimaSnake(){
+        Snake aux = inicio;
+        while(aux.getProximo() != null){
+            aux = aux.getProximo();
+        }
+        return aux;
+    }
+    
+    public Snake validaCorpo(Snake aux){
+        Snake aux2 = inicio;
+        if(aux == aux2){
+            return null;
+        }else{
+            return aux;
+        }
+    }
+    
+    public Snake getCabeca(){
+        return inicio;
+    }
+    
+    public void moveCabeca(int _x, int _y){
+        if(inicio.getX()>=750 || inicio.getY()> 550 || inicio.getX()==0 || inicio.getY()==0){
+            Board fim = new Board();
+            fim.setIsPlaying(false);
+           
+        }else{
+            inicio.setX(_x);
+            inicio.setY(_y);
+        }
+        System.out.println("X:"+inicio.getX()+" Y:"+inicio.getY());
     }
 }
